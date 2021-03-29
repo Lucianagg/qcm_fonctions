1d0166fcd11e36ff5132b65c2e9a20844759c4c3
Lucianagg <lucianagolvet28@gmail.com>
2021-03-29 13:54:07 +0200

    1354

--- exo de boucles/boucles.js
+++ exo de boucles/boucles.js
@@ -1,106 +1,179 @@
  
- //exercice 1
-    for (let i=0; i<50 ; i++) {
-        console.log("Bonjour");
-}
+//  //exercice 1
+//     for (let i=0; i<50 ; i++) {
+//         console.log("Bonjour");
+// }
 
 
 
-//exercice 2 
+// //exercice 2 
 
-for (let i=0; i<5; i++) {
-    console.log("A");
-    console.log("B");
+// for (let i=0; i<5; i++) {
+//     console.log("A");
+//     console.log("B");
 
-}   
+// }   
 
-//exercice 3
-
-
-for (let i=0; i<3; i++){
+// //exercice 3
 
 
-    for (let j=0; j<5; j++){
-        console.log("A");
-    }
+// for (let i=0; i<3; i++){
+
+
+//     for (let j=0; j<5; j++){
+//         console.log("A");
+//     }
     
-    console.log("B")  
-}
+//     console.log("B")  
+// }
 
 
-//exercice 4
+// //exercice 4
 
 
-for (let i=0; i<1; i++){
-    console.log("0");
-    console.log("1");
-    console.log("2");
-    console.log("3");
-    console.log("4");
-    console.log("5");
-    console.log("6");
-    console.log("7");
-    console.log("8");
-    console.log("9");
+// for (let i=0; i<1; i++){
+//     console.log("0");
+//     console.log("1");
+//     console.log("2");
+//     console.log("3");
+//     console.log("4");
+//     console.log("5");
+//     console.log("6");
+//     console.log("7");
+//     console.log("8");
+//     console.log("9");
 
-}
-
-
-//exercice 5
-
-for (let i=0; i<1; i++){      //  AUTRE METHODE for(let i=3; i<12; i++){         // for (let i=0 ; i<9), i++{
-        console.log("3");               //      console.log(i);                     console.log(i+3);
-        console.log("4");               //}
-        console.log("5");
-        console.log("6");
-        console.log("7");
-        console.log("8");
-        console.log("9");
-        console.log("10");
-        console.log("11");
-}
-
-//exercice 6
-
-for (let i=0; i<2; i++){
-        console.log("0");
-        console.log("1");
-        console.log("2");
-        console.log("3");
-
-}
-console.log("0");
-console.log("1");
+// }
 
 
-//exercice 7
+// //exercice 5
+
+// for (let i=0; i<1; i++){      //  AUTRE METHODE for(let i=3; i<12; i++){         // for (let i=0 ; i<9), i++{
+//     console.log("3");               //      console.log(i);                     console.log(i+3);
+//     console.log("4");               //}
+//     console.log("5");
+//     console.log("6");
+//     console.log("7");
+//     console.log("8");
+//     console.log("9");
+//     console.log("10");
+//     console.log("11");
+// }
+
+// //exercice 6
+
+// IL EXISTE PLUSIEURS METHODES:           
  
-for (let i=0; i<1; i++){
-     console.log("0");
-     console.log("1");
-     console.log("2");
-     console.log("3");
-     console.log("4");
+// for (let i=0; i<2; i++){                   
+//         console.log("0");
+//         console.log("1");
+//         console.log("2");
+//         console.log("3");
+
+
+
+// }
+// console.log("0");
+// console.log("1");
+
+//Autre methode:
+
+// for let i=0; i<10; i++{
+//     console.log(i%4);
+//     console.log(i,i-4);
+//     console.log(0,0-4);
+//     console.log(1,1-4);
+//     console.log(2,2-4);
+//     console.log(3,-4);
+//     console.log(4,-4);
+// }
+ //Autre méthode:
+
+for (let i=0; i<10; i++) {
+    console.log(i%4);
+    console.log(0%4);
+    console.log(1%4);
+    console.log(2%4);
+    console.log(3%4);
+    console.log(4%4);
+    console.log(5%4);
+    console.log(6%4);
+    console.log(7%4);
+    console.log(8%4);
+    console.log(9%4);
+}
+
+
+
+// //exercice 7
+ 
+// for (let i=0; i<1; i++){             
+//      console.log("0");               
+//      console.log("1");
+//      console.log("2");
+//      console.log("3");
+//      console.log("4");
     
-}
+// }
 
-for (let i=0; i<3; i++){
-      console.log("A");
+// for (let i=0; i<3; i++){
+//       console.log("A");
       
+// }
+
+// for (let i=0; i<1; i++){
+//     console.log("8");
+//     console.log("9");
+// }
+
+/*
+for (let i =0; i<10; i++){
+ if compris entre 4 et 8
+ if(0>4 && 0<8){
+}
+ console.log("A");
+ 
+}else{
+    console.log(i);
 }
 
-for (let i=0; i<1; i++){
-    console.log("8");
-    console.log("9");
+
+// // exercice 8
+
+// for (let i=0; i<3; i++){
+//     for (let j=0; j<3; j++){
+//         console.log("100");
+//         console.log("1");
+//         console.log("2");
+//     }    
+// } 
+//exercice 13
+
+
+// for (let i=0; i<1; i++){   //Autre méthode:
+//     console.log(1*1);
+//     console.log(1*2);
+//     console.log(1*3);
+//     console.log(1*4);
+//     console.log(1*5);
+//     console.log(1*6);
+//     console.log(1*7);
+//     console.log(1*8);
+//     console.log(1*9);
+
+// }
+
+//exercice 14
+for (let i=2; i>10; i++){
+    console.log(0);
+    console.log(1);
+    console.log(2);
 }
 
 
 
-// exercice 8
 
-for (let i=0; i<3; i++){
 
-}
 
 
 /*
