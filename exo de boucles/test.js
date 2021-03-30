cd6e5c6c6653a0c59355056ec9c5e4efd0a964ff
Lucianagg <lucianagolvet28@gmail.com>
2021-03-30 16:58:47 +0200

    1658

---
+++ exo de boucles/test.js
@@ -0,0 +1,102 @@
+// function fTest(n,name) {
+//     for(let i = 0 ; i < n; i++) {
+//         console.log(name);
+//     }
+// }
+
+// fTest(5, "Pierre"); //Dans la console pierre sera répété 5 fois 
+// fTest(10,"vhdjqk"); // répété 10 fois 
+
+// //DECLARER C EST CREER UNE VARIABLE 
+
+// //  return : j ai termine j arrete la fonction 
+
+//function f2(){
+  //   return 17;
+ //}
+
+ //Afficher dans la console la fonction f2
+ //console.log(f2);
+
+
+
+// //Afficher dans la console le résultat de l'appel de la fonction f2. ( sans taper le 17)
+// console.log(f2 ());
+
+
+ //EXERCICE 
+
+// Écrire une fonction f1 qui ne prend rien en entrée, et qui affiche 17 dans la console.
+// Écrire une fonction f2 qui ne prend rien en entrée, et qui retourne 17.
+
+// Utiliser la fonction f1 pour afficher 17 dans la console.
+// Utiliser la fonction f2 pour afficher 17 dans la console.
+// La fonction f2 a l'air moins pratique à utiliser. Quel intérêt pourrait-elle avoir par rapport à f1 ?
+
+// Écrire une fonction f3 qui prend un nombre en entrée et qui affiche dans la console le double de ce nombre.
+// Écrire une fonction f4 qui prend un nombre en entrée et qui retourne le double de ce nombre.
+
+// Utiliser la fonction f3 pour écrire dans la console le double de 99.
+// Utiliser la fonction f4 pour écrire dans la console le double de 99.
+// Utiliser la fonction f4 pour stocker dans une variable appelée a le double de 99.
+
+// Écrire une fonction f5 qui prend en entrée deux nombres et qui affiche dans la console la somme de ces deux nombres.
+// Écrire une fonction f6 qui prend en entrée deux nombres et qui retourne la somme de ces deux nombres.
+
+// Utiliser la fonction f5 pour écrire dans la console la somme de 42 et 77.
+// Utiliser la fonction f6 pour écrire dans la console la somme de 42 et 77.
+// Utiliser la fonction f6 pour incrémenter la variable a de la somme de 42 et 77.
+
+// Écrire une fonction f7 qui prend deux nombres en entrée et qui retourne le plus grand des deux.
+// Écrire une fonction f8 qui prend trois nombres en entrée et qui retourne le plus grand des trois.
+// (Au moins deux versions possibles : sans utiliser f7, et en utilisant f7).
+
+// Écrire une fonction f9 qui prend un nombre et un mot en entrée, et qui affiche dans la console ce mot ce nombre de fois.
+
+function f1(){
+    console.log(17);
+}
+f1();
+
+
+
+
+function f2(){
+    return 17;
+}
+console.log(f2());
+
+//Son intéret c'est de dire que la fonction est terminée
+
+
+
+
+
+function f3(a){   
+
+console.log(99*2);}
+
+
+function f4(){
+  return 99;  
+}
+console.log(f4);
+
+
+
+
+
+
+
+
+
+
+
+
+// IMPORTANT
+// console.log (f2());
+
+// console.log(17+2); //ça va afficher 19
+// console.log(1+2+"4"); //ça va faire 3*4 et dans la 
+
+
